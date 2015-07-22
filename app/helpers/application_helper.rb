ec70c62fe987d6f03e271988a7d5fc60add48f6e
srbdev <srbdevelopment@gmail.com>
2015-07-21 22:48:29 -0600

    Set up static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # returns the full title on a per page basis
+  def full_title(title = '')
+    base = "Starter App"
+    if title.empty?
+      base
+    else
+      title + " | " + base
+    end
+  end
+
 end
